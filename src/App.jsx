556a786d738c7071fec0c69a654c0050ef0635eb
Prince Yadav <36567474+yyprince334@users.noreply.github.com>
2026-09-24 17:20:44 +0530

    Feature Added: Form
    feature: add team member registration form

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,8 @@
-import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import './App.css'
+import TeamMemberForm from './components/TeamMemberForm';
 
 function App() {
   return (
-    <div>
-      <h1>3S Team Application 🚀</h1>
-      <p>My first React application deployed on Vercel.</p>
-    </div>
+    <TeamMemberForm/>
   );
 }
 
