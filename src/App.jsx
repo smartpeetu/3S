1462315e87de1bd94f8d55c4f855efec39d0b8c0
Prateek Srivastava <prasrivastava@gmail.com>
2026-09-24 13:16:34 +0530

    Create initial React application

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>3S Team Application</h1>
+      <p>My first React application deployed on Vercel.</p>
+    </div>
+  );
+}
+
+export default App;
